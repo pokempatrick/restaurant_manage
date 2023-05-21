7668486b0640a12c2fb3de359fe7c39dfb31123c
Pokem <pokempatrick@yahoo.fr>
2023-05-21 10:27:22 +0100

    mysql connection achieved

--- restaurant_manage/settings.py
+++ restaurant_manage/settings.py
@@ -116,8 +116,15 @@ WSGI_APPLICATION = 'restaurant_manage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
